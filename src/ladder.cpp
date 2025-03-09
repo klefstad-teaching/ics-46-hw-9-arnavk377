1bf990eb75fb65c351c031ba4238e87c6810c7d4
Arnav Kanekar <akanekar@uci.edu>
2025-03-09 20:18:33 +0000

    initial ladder code

---
+++ src/ladder.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+#include <string>
+#include <cmath>
+#include "ladder.h"
+
+using namespace std;
+
+void error(string word1, string word2, string msg) {
+    cerr << msg << endl;
+    cerr << "Caused by: " << word1 << ", " << word2 << endl;
+}
+
+bool edit_distance_within(const std::string& str1, const std::string& str2, int d);
+
+bool is_adjacent(const string& word1, const string& word2);
+
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list);
+
+void load_words(set<string> & word_list, const string& file_name) {
+    ifstream in(file_name);
+    string word;
+    while (in >> word) {
+        word_list.insert(word);
+    }
+}
+
+void print_word_ladder(const vector<string>& ladder) {
+    for (auto word: ladder) {
+        cout << word << ' ';
+    }
+}
+
+void verify_word_ladder();
