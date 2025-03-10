059bd7a08d40f4e3afb8060fee1d1cacfb5245eb
Arnav Kanekar <akanekar@uci.edu>
2025-03-10 01:01:24 +0000

    dijkstras

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,3 +1,41 @@
-vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous);
-vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination);
-void print_path(const vector<int>& v, int total);
+vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
+    int numVertices = G.size();
+    vector<int> distances(numVertices, INF);
+    vector<bool> visited(numVertices, false);
+    distances[source] = 0;
+    previous[source] = NULL;
+    priorityqueue <pair<int, int>> minHeap;
+    minHeap.push({source, 0});
+    while (!minHeap.empty()) {
+        int u = minHeap.top().first;
+        minHeap.pop();
+        if (visited[u] == true) { continue; }
+        visited[u] = true;
+        for (auto e: G[u]) {
+            int v = e.dst;
+            int weight = e.weight;
+            if (!visited[v] && distances[u] < distances[v]) {
+                distances[v] = distances[u] + weight;
+                previous[v] = u;
+                minHeap.push({v, distances[v]});
+            }
+        }
+    }
+    return distances;
+}
+
+vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
+    vector<int> path;
+    for (int at = destination; at != -1; at = previous[at]) {
+        path.push_back(at);
+    }
+    reverse(path.begin(), path.end());
+    return path;
+}
+
+void print_path(const vector<int>& v, int total) {
+    cout << total << endl;
+    for (auto item: v) {
+        cout << v << ' ';
+    }
+}
--- src/ladder.cpp
+++ src/ladder.cpp
@@ -17,7 +17,9 @@ void error(string word1, string word2, string msg) {
 
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d);
 
-bool is_adjacent(const string& word1, const string& word2);
+bool is_adjacent(const string& word1, const string& word2) {
+    if word1.length()
+}
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list);
 
@@ -27,6 +29,7 @@ void load_words(set<string> & word_list, const string& file_name) {
     while (in >> word) {
         word_list.insert(word);
     }
+    in.close();
 }
 
 void print_word_ladder(const vector<string>& ladder) {
