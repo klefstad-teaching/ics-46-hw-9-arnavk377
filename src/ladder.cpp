957030006106efd1ce3379fac521703c53c9698a
Arnav Kanekar <akanekar@uci.edu>
2025-03-11 19:03:18 +0000

    changes

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -46,29 +46,25 @@ bool is_adjacent(const string& word1, const string& word2) {
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
     if (begin_word == end_word) { return {}; }
     if (word_list.find(end_word) == word_list.end()) { return {}; }
+
     queue<vector<string>> q;
-    set<string> visited;  
     q.push({begin_word});
+    set<string> visited;
     visited.insert(begin_word);
+
     while (!q.empty()) {
-        int q_size = q.size();
-        set<string> q_visited;
-        for (int i = 0; i < q_size; i++) {
-            vector<string> path = q.front();
-            q.pop();
-            string last = path.back();
-            if (last == end_word) { return path; }
-            for (auto word: word_list) {
-                if (visited.find(word) == visited.end() && is_adjacent(last, word)) {
-                    vector<string> new_path = path;
-                    new_path.push_back(word);
-                    q.push(new_path);
-                    q_visited.insert(word);
-                }
+        vector<string> ladder = q.front();
+        q.pop();
+        string last = ladder.back();
+
+        for (auto word: word_list) {
+            if (is_adjacent(last, word) && visited.find(word) == visited.end()) {
+                vector<string> new_path = ladder;
+                new_path.push_back(word);
+                visited.insert(word);
+                if (word == end_word) { return new_path; }
+                q.push(new_path);
             }
-        }
-        for (auto w : q_visited) {
-            visited.insert(w);
         }
     }
     return {};
@@ -93,9 +89,15 @@ void verify_word_ladder() {
     set<string> word_list;
     load_words(word_list, "words.txt");
     my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+    cout << generate_word_ladder("cat", "dog", word_list).size() << endl;
     my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+    cout << generate_word_ladder("marty", "curls", word_list).size() << endl;
     my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+    cout << generate_word_ladder("code", "data", word_list).size() << endl;
     my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+    cout << generate_word_ladder("work", "play", word_list).size() << endl;
     my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+    cout << generate_word_ladder("sleep", "awake", word_list).size() << endl;
     my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+    cout << generate_word_ladder("car", "cheat", word_list).size() << endl;
 }
--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -2,7 +2,12 @@
 #include <string>
 
 int main() {
-    cout << edit_distance_within("bye", "byed", 1) << endl;
-    // verify_word_ladder();
+    // cout << edit_distance_within("bye", "byed", 1) << endl;
+    // cout << edit_distance_within("baed", "byed", 1) << endl;
+    // cout << edit_distance_within("bye", "by", 1) << endl;
+    // cout << edit_distance_within("bye", "ye", 1) << endl;
+
+    verify_word_ladder();
+
     return 0;
 }
