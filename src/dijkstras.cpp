b79902f2a41c8f7029d8e0523c673b2564b56f50
Arnav Kanekar <akanekar@uci.edu>
2025-03-14 00:47:15 +0000

    please work

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -12,19 +12,19 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     vector<bool> visited(numVertices, false);
     distances[source] = 0;
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
-    minHeap.push({source, 0});
+    minHeap.push({0, source});
     while (!minHeap.empty()) {
-        int u = minHeap.top().first;
+        int u = minHeap.top().second;
         minHeap.pop();
         if (visited[u] == true) { continue; }
         visited[u] = true;
         for (auto e: G[u]) {
             int v = e.dst;
             int weight = e.weight;
-            if (!visited[v] && distances[u] + weight < distances[v]) {
+            if (distances[u] + weight < distances[v]) {
                 distances[v] = distances[u] + weight;
                 previous[v] = u;
-                minHeap.push({v, distances[v]});
+                minHeap.push({distances[v], v});
             }
         }
     }
